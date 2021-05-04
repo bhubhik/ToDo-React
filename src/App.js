dc66826975b046a049ad8fe74c5caab3dd4b8579
Anubhab Karki <bhubhik@gmail.com>
2021-05-04 19:23:55 +1000

    React TodoList

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React , {useState, useEffect} from 'react';
+import './App.css';
+import Forms from './components/forms';
+import TodoList from './components/todoList';
+
+function App() {
+  const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [status, setStatus] = useState("all");
+  const [filteredTodos, setFilteredTodos] = useState([]);
+
+ useEffect(() => {
+   filteredResult();
+ }, [todos, status])
+  
+  const filteredResult = () => {
+    switch (status){
+      case "completed" :
+       setFilteredTodos(todos.filter(todo => todo.completed === true));
+      break;
+      case "uncompleted" :
+        setFilteredTodos(todos.filter(todo => todo.completed === false));
+      break;
+      default :
+      setFilteredTodos(todos);
+      break;
+    }
+  }
+
+  return (
+    <div className="App">
+     <header>
+        <h1>Your List :</h1>
+    </header>
+    <Forms inputText={inputText} setInputText={setInputText} todos={todos} setTodos={setTodos} setStatus={setStatus} />
+    <TodoList todos={todos } setTodos={setTodos} todos = {todos}  filteredTodos={filteredTodos}/>
+    </div>
+    
+  );
+}
+
+export default App;
